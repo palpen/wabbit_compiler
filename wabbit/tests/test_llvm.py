8c2f4629710324bbd5c893772ae466114919ecd8
Palermo Penano <palermo.penano@scotiabank.com>
2020-07-26 23:20:12 -0400

    clean up llvm test script

--- wabbit/tests/test_llvm.py
+++ wabbit/tests/test_llvm.py
@@ -6,7 +6,7 @@ from wabbit.parse import *
 from wabbit.typecheck import *
 from wabbit.llvm import *
 
-def run(source, out):
+def run(source):
 
     # Parse wabbit soure code to convert to data model
     print(">>>> Model being run:")
@@ -23,43 +23,14 @@ def run(source, out):
     print(">>> Output of interpreter:")
     print(interpret_program(model))
 
-def test_example0():
+def test_simple_print():
     source = """
         print 60;
         print 50;
     """
-    out = """
-    """
-    print(">>>>> LLVM code:")
-    run(source, out)
+    run(source)
 
-def test_example1():
-    source = """
-        var x int = 4;
-        var y int = 5;
-        print x + y;
-    """
-    out = """
-    ; ModuleID = "wabbit"
-    target triple = "unknown-unknown-unknown"
-    target datalayout = ""
-
-    define i32 @"main"()
-    {
-    entry:
-      %"x" = alloca i32
-      %"y" = alloca i32
-      store i32 4, i32* %"x"
-      store i32 5, i32* %"y"
-      %".4" = load i32, i32* %"x"
-      %".5" = load i32, i32* %"y"
-      %".6" = add i32 %".4", %".5"
-      %"d" = alloca i32
-      store i32 %".6", i32* %"d"
-      %".8" = load i32, i32* %"d"
-      ret i32 %".8"
-    }
-    """
+def test_intfloat_arithmetic():
     source = """
         print 1 + 3;
         print 2 * 3;
@@ -69,6 +40,13 @@ def test_example1():
         print -3 + 10;
         print -3.0 + 10.1;
     """
-    run(source, out)
+    run(source)
 
+def test_vardeclaration():
+    source = """
+        var x int = 4;
+        var y int = 5;
+        print x + y;
+    """
+    run(source)
 
