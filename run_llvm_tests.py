2ad20e4f67721756f2b3355cc7f5a230de0cc8ad
Palermo Penano <palermo.penano@scotiabank.com>
2020-07-28 22:41:13 -0400

    add tests

--- run_llvm_tests.py
+++ run_llvm_tests.py
@@ -3,9 +3,11 @@ from wabbit.tests.test_llvm import *
 if __name__ == '__main__':
     #test_simple_print()
     #test_intfloat_arithmetic()
+    test_vardeclaration()
+    test_vardeclaration2()
+    test_vardeclaration3()
 
     # TODO
-    test_vardeclaration()
     # test_ifstatement()
     # test_whileloop()
 
